5bb375abef87ae1b744b6481ccd9f03247575151
Sreeram <ssreeram01@gmail.com>
2022-04-15 16:36:15 -0700

    style changes

--- sreeram/randseq.py
+++ sreeram/randseq.py
@@ -2,19 +2,25 @@
 import argparse
 import random
 
-arg_parser = argparse.ArgumentParser(description='Generate random sequences in a FASTA file format')
+arg_parser = argparse.ArgumentParser(
+    description='Generate random sequences in a FASTA file format')
 
-arg_parser.add_argument('-num_seqs', required=False, type= int, default=20, metavar='num_of_seqs', help='enter the number of seequences you want to generate (defaults to 20')
+arg_parser.add_argument('-num_seqs', required=False, type=int, default=20, metavar='num_of_seqs',
+                        help='enter the number of seequences you want to generate (defaults to 20')
 
-arg_parser.add_argument('-len_each' ,required=False, type= int, default=50, metavar='len_each_seq', help='enter the length of a sequence (defaults to 50)')
+arg_parser.add_argument('-len_each', required=False, type=int, default=50,
+                        metavar='len_each_seq', help='enter the length of a sequence (defaults to 50)')
 
-arg_parser.add_argument('-probabilities', required=False, type= float, nargs=4, default=[0.25, 0.25, 0.25, 0.25], metavar='prob_of_nucleotides', help='enter the probabilities of each type of nucleotide in an array [A, C, G, T] (defauts to 25% each)')
+arg_parser.add_argument('-probabilities', required=False, type=float, nargs=4, default=[
+                        0.25, 0.25, 0.25, 0.25], metavar='prob_of_nucleotides', help='enter the probabilities of each type of nucleotide in an array [A, C, G, T] (defauts to 25% each)')
 
-arg_parser.add_argument('-seed', required=False, default=random.seed(10), type=float, metavar='seed', help='enter the random seed')
+arg_parser.add_argument('-seed', required=False, default=random.seed(10),
+                        type=float, metavar='seed', help='enter the random seed')
 
 entered_args = arg_parser.parse_args()
 
 for each in range(entered_args.len_each):
     print(f'>id{each+1}')
-    seq = ''.join(random.choices('ACGT', weights=entered_args.probabilities, k=entered_args.len_each))
+    seq = ''.join(random.choices(
+        'ACGT', weights=entered_args.probabilities, k=entered_args.len_each))
     print(seq)
--- sreeram/smith_waterman.py
+++ sreeram/smith_waterman.py
@@ -9,7 +9,8 @@ inputs are the two sequences, match score (default value of 3), and a gap cost t
 output is the scoring matrix of the two sequences
 """
 
-def scoring_matrix(seq_1, seq_2, match_score = 3, gap_cost = 2):
+
+def scoring_matrix(seq_1, seq_2, match_score=3, gap_cost=2):
     # make an empty matrix using numpy arrays and the length of the two sequences
     matrix_return = np.zeros((len(seq_1) + 1, len(seq_2) + 1), np.int)
 
@@ -17,16 +18,18 @@ def scoring_matrix(seq_1, seq_2, match_score = 3, gap_cost = 2):
     for i, j in itertools.product(range(1, matrix_return.shape[0]), range(1, matrix_return.shape[1])):
 
         # if the sequences have a match at that spot
-        match = matrix_return[i-1, j-1] + (match_score if seq_1[i-1] == seq_2[j-1] else - match_score)
+        match = matrix_return[i-1, j-1] + \
+            (match_score if seq_1[i-1] == seq_2[j-1] else - match_score)
         # if the seqences need a deletion at the spot
         delete = matrix_return[i-1, j] - gap_cost
         # if the sequences need an insertion at the spot
         insert = matrix_return[i, j-1] - gap_cost
 
         # fill in the spot in the matrix with the highest of the 3 numbers calculated above or with 0
-        matrix_return[i,j] = max(match, delete, insert, 0)
-    
+        matrix_return[i, j] = max(match, delete, insert, 0)
+
     return matrix_return
+
 
 """
 calculates the backtrace and returns the final sequence and the index where the sequence starts
@@ -36,12 +39,13 @@ inputs are the scoring matrix, the 2nd sequence, return sequence(optional), and 
 outputs are the final sequence after alignment, and the starting index in the 1st sequence where the final sequence is
 """
 
+
 def backtrace(scoring_matrix, seq_2, ret_seq='', old_i=0):
     # we can use np.argmax() to get the FIRST index of the max value in an array
     # we can use this property of np.argmax() by flipping the whole matrix around and using the function to obtain the LAST index of the max value in the array
 
     # flip the scoring matrix
-    scoring_flip  = np.flip(np.flip(scoring_matrix, 0), 1)
+    scoring_flip = np.flip(np.flip(scoring_matrix, 0), 1)
     # get the indices of the max value
     row, col = np.unravel_index(scoring_flip.argmax(), scoring_flip.shape)
     # once you get the indices, subtract to get the true coordinates of the max value
@@ -53,10 +57,12 @@ def backtrace(scoring_matrix, seq_2, ret_seq='', old_i=0):
 
     # if score not 0 then we need to add a gap in the sequence and continue
     # note that we are using seq_2 in order to form the final sequence, so it needs to be one of the inputs
-    ret_seq = seq_2[col_last-1] + '-' + ret_seq if old_i - row_last > 1 else seq_2[col_last-1] + ret_seq
+    ret_seq = seq_2[col_last-1] + '-' + ret_seq if old_i - \
+        row_last > 1 else seq_2[col_last-1] + ret_seq
 
     # if not return before, then we move on to the "next" row
     return backtrace(scoring_matrix[0:row_last, 0:col_last], seq_2, ret_seq, row_last)
+
 
 """
 performs the smith waterman algorithm and returns the local alignment
@@ -65,6 +71,8 @@ inputs are the two sequences, match score (optional and defaults to 3), and a ga
 
 output is the final aligned sequence and the indices where you can find the alignment in the other sequence (seq 1)
 """
+
+
 def smith_waterman(seq_1, seq_2, match_score=3, gap_cost=2):
     seq_1, seq_2 = seq_1.upper(), seq_2.upper()
     score_matrix = scoring_matrix(seq_1, seq_2, match_score, gap_cost)
@@ -74,11 +82,10 @@ def smith_waterman(seq_1, seq_2, match_score=3, gap_cost=2):
     return final_seq, pos, pos+len(final_seq)
 
 
-
 seq_1 = 'GGTTGACTA'
 seq_2 = 'TGTTACGG'
 
 print(scoring_matrix('GGTTGACTA', 'TGTTACGG'))
-final_seq, start, end = smith_waterman(seq_1,seq_2)
+final_seq, start, end = smith_waterman(seq_1, seq_2)
 print(final_seq)
 print(seq_1[start:end])
