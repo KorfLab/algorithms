94291fd05f76a6eb0b7c7024a09d2a8311912e8d
iankorf <ifkorf@gmail.com>
2022-04-15 22:13:02 -0700

    tidy

--- perl/test.pl
+++ perl/test.pl
@@ -4,22 +4,22 @@ use warnings;
 my @TEST = (
 	{
 		name => "randomseq",
-		cli => "./randomseq 100 500 | wc -c",
+		cli  => "./randomseq 100 500 | wc -c",
 		pass => "51490",
 	},
 	{
 		name => "kmerfreq",
-		cli => "./kmerfreq data/testdb.fa.gz | md5sum | cut -f1 -d' '",
+		cli  => "./kmerfreq data/testdb.fa.gz | md5sum | cut -f1 -d' '",
 		pass => "3debb76c314fa3fe51fcac2952d2eebd",
 	},
 	{
 		name => "longestorf",
-		cli => "./longestorf data/testdb.fa.gz | md5sum | cut -f1 -d' '",
+		cli  => "./longestorf data/testdb.fa.gz | md5sum | cut -f1 -d' '",
 		pass => "28b6183998f5c4834d9d4c2a2b1c414f",
 	},
 	{
 		name => "dust",
-		cli => "./dust data/testdb.fa.gz | md5sum | cut -f1 -d' '",
+		cli  => "./dust data/testdb.fa.gz | md5sum | cut -f1 -d' '",
 		pass => "9154561d8a4ca4c7377be87d0e3f6cf0",
 	},
 );
