818e1652d22472f9185729592788ccfb1097be82
AlanAloha <zby991023@gmail.com>
2022-05-16 21:20:46 -0700

    need json

---
+++ alan/go/kmerfreq/go.mod
@@ -0,0 +1,7 @@
+module github.com/AlanAloha/kmerfreq
+
+go 1.16
+
+replace github.com/AlanAloha/read_fasta => ../read_fasta
+
+require github.com/AlanAloha/read_fasta v0.0.0-00010101000000-000000000000
---
+++ alan/go/kmerfreq/kmerfreq.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"github.com/AlanAloha/read_fasta"
+	"flag"
+	"sort"
+	"fmt"
+	"os"
+)
+
+func main() {
+	fasta := flag.String("in", "", "path to fasta file (required)")
+	k := flag.Int("k", 3, "k-mer size")
+	j := flag.Bool("j", false, "output in json (default: tab-separated)")
+	flag.Parse()
+	
+	if *fasta == "" {
+		flag.Usage()
+		os.Exit(1)
+	}
+	
+	fmt.Println(*fasta, *k, *j)
+	
+	total := 0.0
+	freq := make(map[string]float64)
+	
+	var kmer string
+	read_fasta.Read_fasta(*fasta, func (read read_fasta.Read) {
+		//fmt.Println(read.Id)
+		//fmt.Println(read.Seq)
+		for i := 0; i < (len(read.Seq) - *k + 1); i++ {
+			kmer = read.Seq[i:i+*k]
+			freq[kmer] += 1
+			total += 1
+		}
+	})
+	
+	for kmer := range freq {freq[kmer] /= total}
+	
+	//create a slice to sort the keys
+	kmers := make([]string, 0)
+	for kmer := range freq {kmers = append(kmers, kmer)}
+	sort.Strings(kmers)
+	
+	
+	if *j {
+	
+	} else {
+		for _, kmer := range kmers {
+			fmt.Printf("%s\t%.5f\n", kmer, freq[kmer])
+		}
+	}
+
+	
+	
+	
+
+}
