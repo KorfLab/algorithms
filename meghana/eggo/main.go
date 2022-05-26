0bef9192f2fd77a906bc2a1c38232e76e046ad41
Meghana Manepalli <meghymail@gmail.com>
2022-05-26 11:54:08 -0700

    update

--- meghana/eggo/main.go
+++ meghana/eggo/main.go
@@ -2,11 +2,8 @@ package main
 
 import (
 	"flag"
-	"fmt"
-	"time"
+
 	
-	"eggo/readfasta"
-	"eggo/randomseq"
 	"eggo/sireadfasta"
 )
 
@@ -14,7 +11,6 @@ import (
 func main() {	
 	fs := flag.String("f", "", "path to file")
 	flag.Parse()
-	fmt.Println(*fs)
 
 	
 	it := sireadfasta.NewFastaStatefulIterator(fs)
@@ -23,25 +19,3 @@ func main() {
 	}
 }
 
-
-
-//ignore below
-func usereadfasta() {
-	fs := flag.String("f", "", "path to file")
-	flag.Parse()
-	
-	readfasta.Readfasta(*fs, func(fasta readfasta.Fasta) {
-		readfasta.Print(&fasta)
-	})
-}
-
-func userandomseq() {
-	n := flag.Int("n", 10, "number of sequences")
-	l := flag.Int("l", 80, "length of each sequence")
-	p := flag.String("p", ".25, .25, .25, .25", "comma separated values for ACGT")
-	prefix := flag.String("pre", "id", "prefix for sequence identifiers")
-	s := flag.Int64("seed", time.Now().UnixNano(), "random seed")
-	flag.Parse()
-	randomseq.Randomseq(*n, *l, *p, *prefix, *s)
-	
-}
