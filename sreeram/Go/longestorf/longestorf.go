572d69969b1d2f18ce19490fc56a438180a553a1
Sreeram <ssreeram01@gmail.com>
2022-05-26 23:34:42 -0700

    in progress

---
+++ sreeram/Go/longestorf/longestorf.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"strings"
+)
+
+func find_starts(seq string) []int {
+	start_indices := []int{}
+	cur_start := 0
+
+	cur_start = strings.Index(seq, "ATG")
+
+	return start_indices
+}
