2891a9bc4cc46df08da8d2d4bc2814cf487c7ac0
mslilyb <lilith.r.m.d@gmail.com>
2022-07-27 10:33:42 -0700

    directory cleanup

--- lilith/grimoire2/randomseq/randomseq.go
+++ lilith/grimoire2/randomseq/randomseq.go
@@ -2,49 +2,59 @@
 // it to the console. Users should ensure that their nucleotide frequencies sum
 // one as much as possible.
 
+
 package randomseq
 
 import (
-	//"error"
-	"fmt"
-	//"math/rand"
-	//"sort"
+  //"error"
+  "fmt"
+  //"math/rand"
+  //"sort"
 )
+
 
 //Choice pairs a given string to a weight.
 type Choice struct {
-	Option string
-	Weight float32
+  Option string
+  Weight float32
+}
+
+type Chooser struct {
+  Choices []Choice
+  Breakpoints []float32
+  Maxweight float32
 }
 
 // This function will create a new Choice based on the dictionary and the weights
 // input by the user.
 func NewChoice(option string, weight float32) Choice {
-	return Choice{Option: option, Weight: weight}
+  return Choice{Option: option, Weight: weight}
 }
 
 // Creates a Chooser object, which sorts the choices in ascending order of their
 // weights to prepare them to generate the sequence.
-func NewChooser(choices []Choice) {
-	//  sort.Slice(choices, func(i, j) int) bool {
-	//    return choices[i].Weight < choices[j].Weight
-	//  }
-	//breakpoints := make([]float32, len(choices))
-	var total float32 = 0.00
+func NewChooser(choices []Choice) (*Chooser) {
+//  sort.Slice(choices, func(i, j) int) bool {
+//    return choices[i].Weight < choices[j].Weight
+//  }
+  breakpoints := make([]float32, len(choices))
+  var total float32 = 0.00
 
-	for i, j := range choices {
-		weight := j.Weight
-		total += choices[i].Weight
-		fmt.Println(weight, total)
-	}
+
+  for i, nt := range choices {
+    breakpoints[i] = nt.Weight
+    total += choices[i].Weight
+  }
+
+  return &Chooser{Choices: choices, Breakpoints: breakpoints, Maxweight: total}
 }
 
-func Run() {
-	demos := make([]Choice, 4)
-	demos[0] = Choice{"A", 0.10}
-	demos[1] = Choice{"C", 0.20}
-	demos[2] = Choice{"G", 0.30}
-	demos[3] = Choice{"T", 0.40}
+func MakeChoices () (*Chooser, error) {
+  demos := make([]Choice, 4)
+  demos[0] = Choice{"A", 0.10}
+  demos[1] = Choice{"C", 0.20}
+  demos[2] = Choice{"G", 0.30}
+  demos[3] = Choice{"T", 0.40}
 
-	NewChooser(demos)
+  weighted := NewChooser(demos)
 }
