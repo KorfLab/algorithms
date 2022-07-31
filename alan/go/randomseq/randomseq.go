60c55b393f5a35202e772365f47c81d4b38c394b
AlanAloha <zby991023@gmail.com>
2022-07-31 11:32:34 -0700

    weights and error

--- alan/go/randomseq/randomseq.go
+++ alan/go/randomseq/randomseq.go
@@ -1,18 +1,29 @@
 package main
 
 import (
+	"math/rand"
+	"strconv"
+	"time"
 	"flag"
 	"fmt"
-	"math/rand"
-	"time"
-	"strconv"
+	"os"
 )
 
-func make_seq(length int) string{
-	bases := "ACGT"
+func make_seq(length int, a float64, c float64, g float64, t float64) string{
 	seq := ""
 	for i := 0; i < length; i++ {
-		seq += string(bases[rand.Intn(4)])
+		rf := rand.Float64()
+		var nb string
+		if rf < a {
+			nb = "A"
+		} else if rf < a + c {
+			nb = "C"
+		} else if rf < a + c + g {
+			nb = "G"
+		} else {
+			nb = "T"
+		}
+		seq += nb
 	}
 	return seq
 }
@@ -22,11 +33,17 @@ func main() {
 	len_seq := flag.Int("len", 100, "length of sequence")
 	prefix := flag.String("prefix", "id", "prefix for identifier")
 	seed := flag.String("seed", "random", "random seed" )
-	// For composition of bases:
-	// option 1: one float flag for each base (e.g. -a 0.25 -c 0.25 -g 0.25 -t 0.25)
-	// option 2: one string flag for all bases (e.g. -comp "0.25 0.25 0.25 0.25")
+	a := flag.Float64("a", 0.25, "weight of A")
+	c := flag.Float64("c", 0.25, "weight of C")
+	g := flag.Float64("g", 0.25, "weight of G")
+	t := flag.Float64("t", 0.25, "weight of T")
 	flag.Parse()
 	
+	if *a + *c + *g + *t - 1.0 > 0.00001 {
+		fmt.Printf("User input:\nA: %f\nC: %f\nG: %f\nT: %f\n", *a, *c, *g, *t)
+		os.Exit(1)
+	}
+
 	if *seed != "random" {
 		seed, err:= strconv.ParseInt(*seed, 0, 64)
 		if err != nil {
@@ -42,7 +59,7 @@ func main() {
 	for i := 0; i < *num_seq; i++ {
 		fmt.Printf(">%s%d\n", *prefix, i+1)
 		
-		seq = make_seq(*len_seq)
+		seq = make_seq(*len_seq, *a, *c, *g, *t)
 		for j := 0; j < len(seq); j += 80 {
 			if j + 80 < len(seq) {
 				fmt.Println(seq[j:j+80])
