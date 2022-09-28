5b3bf3f44ab0237917b42d7c3f5215d9dce19c05
mslilyb <lilith.r.m.d@gmail.com>
2022-09-28 09:18:18 -0700

    initial commit. Todo: transition probs

---
+++ viterbi/lilith/go.mod
@@ -0,0 +1,3 @@
+module viterbi
+
+go 1.18
---
+++ viterbi/lilith/viterbi.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+  "flag"
+  "fmt"
+  "github.com/Korflab/grimoire2/read_record"
+  "math"
+)
+
+
+//creates a simple model, only accounting for one position.
+func makemodel () map[string]float64 {
+  var bases = make(map[string]float64)
+
+  for _, letter := range('ACTG') {
+    bases[letter] = 0.0
+  }
+}
+
+//Turns probabilities into log-odds scores
+func logodds (p float64) float64 {
+  if p == 0 {
+    return math.SmallestNonzeroFloat64
+  } else {
+    return math.Log2(p/0.25)
+  }
+}
+
+
+func trainmodel (file) map[string]float64 {
+  model = makemodel()
+
+  fasta = read_record.Read_fasta(file)
+
+  for fasta.Next() {
+    record := fasta.Current()
+
+    for _, letter := range(record.Seq) {
+      model[letter] += 1.0
+    }
+  }
+  total := 0.0
+
+  for _, count := range model {
+    total += count
+  }
+
+  for letter, count := range model {
+    prob := count / total
+    model[letter] = logodds(prob)
+  }
+
+  return model
+}
+
+
+func main () {
+  flag.StringVar(&infile, "f", "", "query file")
+  flag.StringVar(&posfile, "pos", "positive strand training file")
+  flag.StringVar(&negfile, "neg", "negative strand training file")
+
+  flag.Parse()
+
+  positivemodel = trainmodel(posfile)
+  negativemodel = trainmodel(negfile)
+
+  
+}
