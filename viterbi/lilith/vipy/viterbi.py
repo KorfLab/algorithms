a2cd4d07b84aa043515ace3c1878333d53cc277b
mslilyb <lilith.r.m.d@gmail.com>
2022-10-11 21:39:19 -0700

    progress, still no execute

--- viterbi/lilith/vipy/viterbi.py
+++ viterbi/lilith/vipy/viterbi.py
@@ -24,6 +24,41 @@ posemps = emissionP(posmodel, args.n, True)
 negemps = emissionP(negmodel, args.n, True)
 genoemps = {"A": -2, "C": -2, "T": -2, "G": -2}
 
+emPs = [genoemps, posemps, negemps]
 
-prob = []
+#Transition P
+transP = vt.transPs
+
 trace = []
+
+for name, seq in vt.readfasta(args.fname):
+
+    prob = [[0 for i in range(len(seq) - args.n)] for j in range(len(transP))]
+    trace = []
+
+    #initialize prob matrix
+    prob[0][0] = vt.prob2log(1)
+    prob[0][1] = vt.prob2log(0)
+    prob[0][2] = vt.prob2log(0)
+
+    if args.n != 0:
+        for i in range(arg.n)
+            prob[i][0] = vt.prob2log(1)
+            prob[i][1] = vt.prob2log(0)
+            prob[i][2] = vt.prob2log(0)
+
+    #initialize trace 
+    trace[0] = -1 
+
+    if args.n == 0:
+        for i in range(len(seq)):
+            letter = seq[i]
+            for j in range(len(transP)):
+                maxscore = None
+                for k in range(len(transP)):
+                    score = prob[i][k] + emPs[k][letter] + transP[i][k]
+                    if maxscore = None or score > maxscore:
+                        maxscore = score
+
+
+                prob[i][j] = maxscore
