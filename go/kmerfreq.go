7bd1a7dd36678e421eb356e1415f467de71bafdd
AlanAloha <zby991023@gmail.com>
2023-03-09 02:36:38 -0800

    up

---
+++ go/kmerfreq.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"github.com/AlanAloha/read_record"
+	"flag"
+	"sort"
+	"fmt"
+	"os"
+)
+
+func main() {
+	fasta := flag.String("in", "", "path to fasta file (required)")
+	k := flag.Int("k", 3, "k-mer size (default: k=3)")
+	j := flag.Bool("j", false, "output in json (default: tab-separated)")
+	
+	flag.Usage = func() {
+		flagSet := flag.CommandLine
+		fmt.Printf("Kmer frequency in either tab-separated or json format\n")
+		order := []string{"in", "k", "j"}
+		for _, name := range order {
+			flag := flagSet.Lookup(name)
+			fmt.Printf("-%s\n", flag.Name)
+			fmt.Printf("    %s\n", flag.Usage)
+		}
+	}
+	
+	flag.Parse()
+	
+	if *fasta == "" {
+		flag.Usage()
+		os.Exit(1)
+	}
+	
+	total := 0.0
+	freq := make(map[string]float64)
+	
+	var kmer string
+	records := read_record.Read_fasta(*fasta)
+	for records.Next() {
+		record := records.Record()
+		for i := 0; i < (len(record.Seq) - *k + 1); i++ {
+			kmer = record.Seq[i:i+*k]
+			freq[kmer] += 1
+			total += 1
+		}
+	}
+	
+	for kmer := range freq {freq[kmer] /= total}
+	
+	//create a slice to sort the keys
+	kmers := make([]string, 0)
+	for kmer := range freq {kmers = append(kmers, kmer)}
+	sort.Strings(kmers)
+	
+	
+	if *j {
+		fmt.Printf("{\n")
+		for i, kmer := range kmers {
+			if i == len(kmers) - 1 {
+				fmt.Printf("\t\"%s\": %f\n", kmer, freq[kmer])
+			} else {
+				fmt.Printf("\t\"%s\": %f,\n", kmer, freq[kmer])
+			}
+		}
+		fmt.Printf("}\n")
+	} else {
+		for _, kmer := range kmers {
+			fmt.Printf("%s\t%f\n", kmer, freq[kmer])
+		}
+	}
+
+}
---
+++ go/longestorf.go
@@ -0,0 +1,156 @@
+package main
+
+import (
+	"github.com/AlanAloha/read_record"
+	"strings"
+	"flag"
+	"fmt"
+	"os"
+)
+
+var CODONS = map[string]string {
+	"AAA" : "K",	"AAC" : "N",	"AAG" : "K",	"AAT" : "N",
+	"AAR" : "K",	"AAY" : "N",	"ACA" : "T",	"ACC" : "T",
+	"ACG" : "T",	"ACT" : "T",	"ACR" : "T",	"ACY" : "T",
+	"ACK" : "T",	"ACM" : "T",	"ACW" : "T",	"ACS" : "T",
+	"ACB" : "T",	"ACD" : "T",	"ACH" : "T",	"ACV" : "T",
+	"ACN" : "T",	"AGA" : "R",	"AGC" : "S",	"AGG" : "R",
+	"AGT" : "S",	"AGR" : "R",	"AGY" : "S",	"ATA" : "I",
+	"ATC" : "I",	"ATG" : "M",	"ATT" : "I",	"ATY" : "I",
+	"ATM" : "I",	"ATW" : "I",	"ATH" : "I",	"CAA" : "Q",
+	"CAC" : "H",	"CAG" : "Q",	"CAT" : "H",	"CAR" : "Q",
+	"CAY" : "H",	"CCA" : "P",	"CCC" : "P",	"CCG" : "P",
+	"CCT" : "P",	"CCR" : "P",	"CCY" : "P",	"CCK" : "P",
+	"CCM" : "P",	"CCW" : "P",	"CCS" : "P",	"CCB" : "P",
+	"CCD" : "P",	"CCH" : "P",	"CCV" : "P",	"CCN" : "P",
+	"CGA" : "R",	"CGC" : "R",	"CGG" : "R",	"CGT" : "R",
+	"CGR" : "R",	"CGY" : "R",	"CGK" : "R",	"CGM" : "R",
+	"CGW" : "R",	"CGS" : "R",	"CGB" : "R",	"CGD" : "R",
+	"CGH" : "R",	"CGV" : "R",	"CGN" : "R",	"CTA" : "L",
+	"CTC" : "L",	"CTG" : "L",	"CTT" : "L",	"CTR" : "L",
+	"CTY" : "L",	"CTK" : "L",	"CTM" : "L",	"CTW" : "L",
+	"CTS" : "L",	"CTB" : "L",	"CTD" : "L",	"CTH" : "L",
+	"CTV" : "L",	"CTN" : "L",	"GAA" : "E",	"GAC" : "D",
+	"GAG" : "E",	"GAT" : "D",	"GAR" : "E",	"GAY" : "D",
+	"GCA" : "A",	"GCC" : "A",	"GCG" : "A",	"GCT" : "A",
+	"GCR" : "A",	"GCY" : "A",	"GCK" : "A",	"GCM" : "A",
+	"GCW" : "A",	"GCS" : "A",	"GCB" : "A",	"GCD" : "A",
+	"GCH" : "A",	"GCV" : "A",	"GCN" : "A",	"GGA" : "G",
+	"GGC" : "G",	"GGG" : "G",	"GGT" : "G",	"GGR" : "G",
+	"GGY" : "G",	"GGK" : "G",	"GGM" : "G",	"GGW" : "G",
+	"GGS" : "G",	"GGB" : "G",	"GGD" : "G",	"GGH" : "G",
+	"GGV" : "G",	"GGN" : "G",	"GTA" : "V",	"GTC" : "V",
+	"GTG" : "V",	"GTT" : "V",	"GTR" : "V",	"GTY" : "V",
+	"GTK" : "V",	"GTM" : "V",	"GTW" : "V",	"GTS" : "V",
+	"GTB" : "V",	"GTD" : "V",	"GTH" : "V",	"GTV" : "V",
+	"GTN" : "V",	"TAA" : "*",	"TAC" : "Y",	"TAG" : "*",
+	"TAT" : "Y",	"TAR" : "*",	"TAY" : "Y",	"TCA" : "S",
+	"TCC" : "S",	"TCG" : "S",	"TCT" : "S",	"TCR" : "S",
+	"TCY" : "S",	"TCK" : "S",	"TCM" : "S",	"TCW" : "S",
+	"TCS" : "S",	"TCB" : "S",	"TCD" : "S",	"TCH" : "S",
+	"TCV" : "S",	"TCN" : "S",	"TGA" : "*",	"TGC" : "C",
+	"TGG" : "W",	"TGT" : "C",	"TGY" : "C",	"TTA" : "L",
+	"TTC" : "F",	"TTG" : "L",	"TTT" : "F",	"TTR" : "L",
+	"TTY" : "F",	"TRA" : "*",	"YTA" : "L",	"YTG" : "L",
+	"YTR" : "L",	"MGA" : "R",	"MGG" : "R",	"MGR" : "R",
+}
+
+func str_reverse(str string) string {
+	rev := []rune(str)
+	for i, j := 0, len(rev) - 1; i < j; i, j = i + 1, j - 1{
+		rev[i], rev[j] = rev[j], rev[i]
+	}	
+	
+	return string(rev)	
+}
+
+func revcomp(seq string) string {
+	from := "ACGTRYMKWSBDHVN"
+	to   := "tgcayrkmwsvhdbn"
+	
+	revcomp := strings.ToUpper(seq)
+	for i := 0; i < len(from); i++ {
+		revcomp = strings.ReplaceAll(revcomp, string(from[i]), string(to[i]))
+	}
+	revcomp = strings.ToUpper(revcomp)
+	
+	return str_reverse(revcomp)
+}
+
+func longestPep(seq string, ic bool) string {
+	// pad seq for incomplete
+	seq = seq + strings.Repeat("X", 3-len(seq)%3)
+	
+	longest_pep := ""
+	for i := 0; i < len(seq) - 2; i++ {
+		aa, is_codon := CODONS[seq[i:i+3]]
+		if is_codon && aa == "M" {
+			pep := ""
+			for j := i; j < len(seq) - 2; j += 3 {
+				aa, is_codon := CODONS[seq[j:j+3]]
+				if !is_codon {
+					pep += "X"
+				} else if aa == "*" {
+					if len(pep) > len(longest_pep) {
+						longest_pep = pep	
+				 	}
+					break
+				} else {
+					pep += aa
+				}
+			}
+			if ic && (len(pep) >= 0) {
+				if len(pep) > len(longest_pep) {
+					longest_pep = pep
+				}
+			}
+			
+		}
+	}
+	
+	if len(longest_pep) == 0 {longest_pep += "*"}
+	return longest_pep
+}
+
+func out(seq string) {
+	for i:= 0; i < len(seq); i+=80 {
+		if i+80 >= len(seq) {
+			fmt.Println(seq[i:])
+			break
+		} else {
+			fmt.Println(seq[i:i+80])
+		}	
+	}
+}
+
+func main() {
+	fasta := flag.String("in", "", "path to fasta file (required)")
+	r := flag.Bool("r", false, "also translate reverse complement strand (defaut: false)")
+	ic:= flag.Bool("ic", false, "allow incomplete peptides (defaut: false)")
+	flag.Parse()
+	
+	if *fasta == "" {
+		flag.Usage()
+		os.Exit(1)
+	}
+	
+	records := read_record.Read_fasta(*fasta)
+	for records.Next() {
+		record := records.Record()
+		fmt.Printf(">%s\n",record.Id)
+		seq := record.Seq
+		if *r {
+			revseq := revcomp(seq)
+			fow := longestPep(seq, *ic)
+			rev := longestPep(revseq, *ic)
+			if len(fow) > len(rev) {
+				out(fow)
+			} else {
+				out(rev)
+			}
+		} else {
+			out(longestPep(seq, *ic))
+		}
+	}
+
+}
